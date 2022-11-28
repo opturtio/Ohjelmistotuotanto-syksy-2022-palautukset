589219cedc5e63c0113bb7460089760a3df45822
Olli-Pekka Turtio <olli-pekka.turtio@helsinki.fi>
2022-11-28 11:56:17 +0200

    first test passed

---
+++ viikko4/tdd-ostoskori/src/ostoskori.py
@@ -0,0 +1,38 @@
+from tuote import Tuote
+from ostos import Ostos
+
+class Ostoskori:
+    def __init__(self):
+        # ostoskori tallettaa Ostos-oliota, yhden per korissa oleva Tuote
+        self._ostokset = []
+
+    def tavaroita_korissa(self):
+        pass
+        # kertoo korissa olevien tavaroiden lukumäärän
+        # eli jos koriin lisätty 2 kpl tuotetta "maito", tulee metodin palauttaa 2
+        # samoin jos korissa on 1 kpl tuotetta "maito" ja 1 kpl tuotetta "juusto",
+        # tulee metodin palauttaa 2
+
+    def hinta(self): # kertoo korissa olevien ostosten yhteenlasketun hinnan
+        summa = 0
+        for ostos in self._ostokset:
+            summa += ostos.hinta()
+        return summa
+
+    def lisaa_tuote(self, lisattava: Tuote):
+        # lisää tuotteen
+        self._ostokset.append(lisattava)
+
+    def poista_tuote(self, poistettava: Tuote):
+        # poistaa tuotteen
+        self._ostokset.remove(poistettava)
+
+    def tyhjenna(self):
+        self._ostokset = []
+        # tyhjentää ostoskorin
+
+    def ostokset(self):
+        pass
+        # palauttaa listan jossa on korissa olevat ostos-oliot
+        # kukin ostos-olio siis kertoo mistä tuotteesta on kyse
+        # JA kuinka monta kappaletta kyseistä tuotetta korissa on
