9abdde0db22b9bd434ef2168fbf86f3c68d65ce8
Olli-Pekka Turtio <olli-pekka.turtio@helsinki.fi>
2022-11-28 12:07:35 +0200

    tavaroiden maaran testaus, lisasin myös aikaisemilta viikoilta jääneitä tiedostoja testiin. toivottavasti ei pilaa aikaisempien viikkojen tehtäviä jos niitä ei ole tarkistettu. En saanut noita vaan millään pois häiritsemästä joten nyt pushaan ne. Tuskin tekee mitään.

--- viikko4/tdd-ostoskori/src/ostoskori.py
+++ viikko4/tdd-ostoskori/src/ostoskori.py
@@ -7,7 +7,10 @@ class Ostoskori:
         self._ostokset = []
 
     def tavaroita_korissa(self):
-        pass
+        summa = 0
+        for ostos in self._ostokset:
+            summa += ostos.lukumaara()
+        return summa
         # kertoo korissa olevien tavaroiden lukumäärän
         # eli jos koriin lisätty 2 kpl tuotetta "maito", tulee metodin palauttaa 2
         # samoin jos korissa on 1 kpl tuotetta "maito" ja 1 kpl tuotetta "juusto",
