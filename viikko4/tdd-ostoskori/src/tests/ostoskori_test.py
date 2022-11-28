7c4709d973e37d884c834c1f0af66f13fa25b16a
Olli-Pekka Turtio <olli-pekka.turtio@helsinki.fi>
2022-11-28 19:15:55 +0200

    task 5 done

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -101,3 +101,22 @@ class TestOstoskori(unittest.TestCase):
 
         self.assertEqual(ostos, ('Maito',1))
 
+    def test_kahden_saman_lisays_toisen_poisto_yksi_tuote_jaa(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+        self.kori.poista_tuote(maito)
+
+        ostokset = self.kori.ostokset()
+
+        self.assertEqual(len(ostokset), 0)
+
+    def test_metodi_tyhjenna_tyhjentaa_korin(self):
+        maito = Tuote("Maito", 3)
+        juusto = Tuote("Juusto", 4)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(juusto)
+
+        ostokset = self.kori.ostokset()
+        self.kori.tyhjenna()
+        ostokset = self.kori.ostokset()
+        self.assertEqual(len(ostokset), 0)
